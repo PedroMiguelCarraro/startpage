a7fe7ffa7ac268cc98ed27fb0c8b7f2ed8223267
Pedro Miguel A Carraro <pedromiguel.carraro@gmail.com>
2021-09-28 21:59:12 -0300

    Links and Readme

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ This is a simple, minimal startpage I customized to my needs.
 ![Preview](assets/img/preview.png)
 
 # Credits
-This is a fork of [WitherCubes's minimal startpage](https://github.com/WitherCubes/startpage), wich is a fork of [Wolfiy's startpage](https://gitlab.com/wolfiy/wlfys-minimal-startpage) with modification of colors.
+This is a fork of [WitherCubes's minimal startpage](https://github.com/WitherCubes/startpage), with modification of colors.
 
-The art is by [NurAlifSidoel](https://www.deviantart.com/nuralifsidoel/art/Mizuhara-Chizuru-Kanokari-NurAlifSidoel-Style-849828583).
+[Beware this is a loop of forks](https://xkcd.com/1683/)
+
+
+The art is by [エカニス・エニカ (Echanis_Enicha)](https://twitter.com/Echanis_Enicha/status/1436874590295236608).
--- scripts/time.js
+++ scripts/time.js
@@ -1,9 +1,9 @@
 function doDate() {
 	var str = "";
-	var months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+	var months = new Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Decembro");
 	var clock = new Date();
 	var now = new Date();
-	str += months[clock.getMonth()] + " " + now.getDate() + ", " + clock.getFullYear() + " | " + ("0" + now.getHours()).slice(-2) +":" + ("0" + now.getMinutes()).slice(-2) + ":" + ("0" + now.getSeconds()).slice(-2);
+	str +=  now.getDate() + " " + months[clock.getMonth()] + ", " + clock.getFullYear() + " | " + ("0" + now.getHours()).slice(-2) +":" + ("0" + now.getMinutes()).slice(-2) + ":" + ("0" + now.getSeconds()).slice(-2);
 	document.getElementById("date").innerHTML = str;
 }
 setInterval(doDate, 1000);
